fc0f990baf0a30e1a8a86569713ad30f0ef53bab
Eric Heikes <eheikes@gmail.com>
2015-05-25 19:00:14 -0500

    Load all plugins.

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,22 @@
-var Q = require('kew');
+var config = require('config');
+var Q      = require('kew');
 
-var plugins = [];
+// Make sure that the plugins object exists.
+config.plugins = config.plugins || {};
+
+// Load the plugins.
+var plugins = Object.keys(config.plugins).map(function(name) {
+  var pluginConfig = config.plugins[name];
+  var type = pluginConfig.type;
+  if (!type) return;
+  var PluginConstructor = require('./' + type);
+  return new PluginConstructor(name, pluginConfig);
+});
 
 exports.getSuggestions = function() {
   return Q.all(plugins.map(function(plugin) {
     return plugin.getSuggestions();
-  }));
+  })).then(function(results) {
+    return Array.prototype.concat.apply([], results); // flatten results
+  });
 };
